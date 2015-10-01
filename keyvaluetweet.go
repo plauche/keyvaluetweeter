05837951e82e46ecc7edef6b792efaf7718524a0
Ryan Plauche <ryan.plauche@gmail.com>
2015-09-30 20:43:29 -0500

    Refactored to have KeyValueTweeter object.

--- keyvaluetweet.go
+++ keyvaluetweet.go
@@ -7,30 +7,58 @@ import (
 	"github.com/ChimeraCoder/anaconda"
 )
 
-func Insert(a *anaconda.TwitterApi, key string, value string) anaconda.Tweet {
+type KeyValueTweeter struct {
+	AccountName string
+	Api         *anaconda.TwitterApi
+}
+
+func NewKeyValueTweeter(consumerKey string, consumerSecret string, accessToken string, accessTokenSecret string, accountName string) *KeyValueTweeter {
+	k := new(KeyValueTweeter)
+	k.AccountName = accountName
+	anaconda.SetConsumerKey(consumerKey)
+	anaconda.SetConsumerSecret(consumerSecret)
+	k.Api = anaconda.NewTwitterApi(accessToken, accessTokenSecret)
+	return k
+}
+
+func (k KeyValueTweeter) Insert(key string, value string) anaconda.Tweet {
 	tweetString := fmt.Sprintf("%s #%s", value, key)
-	tweet, err := a.PostTweet(tweetString, nil)
+	tweet, err := k.Api.PostTweet(tweetString, nil)
 	if err != nil {
 		panic(err)
 	}
 	return tweet
 }
 
-func Find(api *anaconda.TwitterApi, key string) string {
-	accountName := "welpwatchout"
-	searchString := fmt.Sprintf("%s #%s", accountName, key)
-	search_result, err := api.GetSearch(searchString, nil)
+func (k KeyValueTweeter) Find(key string) string {
+	searchString := fmt.Sprintf("%s #%s", k.AccountName, key)
+	searchResult, err := k.Api.GetSearch(searchString, nil)
 	if err != nil {
 		panic(err)
 	}
 
 	value := ""
 
-	if len(search_result.Statuses) == 1 {
-		value = search_result.Statuses[0].Text
+	if len(searchResult.Statuses) == 1 {
+		value = searchResult.Statuses[0].Text
 		value = strings.Replace(value, fmt.Sprintf("#%s", key), "", 1)
 		value = strings.TrimSpace(value)
 	}
 
 	return value
+}
+
+func (k KeyValueTweeter) Delete(api *anaconda.TwitterApi, key string) (err error) {
+	searchString := fmt.Sprintf("%s #%s", k.AccountName, key)
+	searchResult, err := k.Api.GetSearch(searchString, nil)
+	if err != nil {
+		panic(err)
+	}
+
+	for _, tweet := range searchResult.Statuses {
+		_, err := k.Api.DeleteTweet(tweet.Id, true)
+		return err
+	}
+
+	return nil
 }
